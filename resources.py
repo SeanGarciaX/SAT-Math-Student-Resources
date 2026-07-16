eb17240a69a7d21c2933c9668aea34faeed29683
Sean Garcia <sean.garcia2900@gmail.com>
2026-07-16 13:20:43 -0400

    Added resources for exponential and quadratic equations

--- resources.py
+++ resources.py
@@ -26,6 +26,22 @@ VIDEO_LINKS = {
         ("Slope Formula", "https://www.youtube.com/watch?v=E5lCvitzOaI"),
         ("Parallel and Perpendicular Lines", "https://www.youtube.com/watch?v=0oe-GqFrYFU"),
     ],
+    "Exponential Equations": [
+        ("Intro to Exponential Functions", "https://www.youtube.com/watch?v=nqpn0SQB5ds"),
+        ("Exponential Growth and Decay Video", "https://www.youtube.com/watch?v=1ZE5EihrtZc"),
+        ("Compound Interest Formula", "https://www.youtube.com/watch?v=U0Lwyh9ONMM&t=5s"),
+    ],
+    "Equation of a Circle": [
+        ("Equation of a Circle Video", "https://www.youtube.com/watch?v=QutKdoLiffc"),
+        ("Solving Equation of a Circle Problems in DESMOS", "https://www.youtube.com/watch?v=Ejzcmgupq-k"),
+    ],
+    "Quadratic Equations": [
+        ("Introduction to Quadratic Equations", "https://www.youtube.com/watch?v=xSpFz8JDbZk"),
+        ("Quadratic Vertex Form", "https://www.youtube.com/watch?v=A2eK7VziEn0&t=90s"),
+        ("Quadratic Factored Form", "https://www.youtube.com/watch?v=pJjfJxX-aHs"),
+        ("Discriminant Rule", "https://www.youtube.com/watch?v=WizUEZSHyWo"),
+        ("Quadratic Formula", "https://www.youtube.com/watch?v=Yytf1CSXZgE"),
+    ],
 }
 
 EXAMPLE_PROBLEMS = {
