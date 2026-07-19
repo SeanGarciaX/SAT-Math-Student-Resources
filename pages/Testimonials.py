ca3cc5ec80d312eb4e6c72bcf164a854b304c80d
Sean Garcia <sean.garcia2900@gmail.com>
2026-07-19 13:22:59 -0400

    Increase max-width of cloud text in Testimonials

--- pages/Testimonials.py
+++ pages/Testimonials.py
@@ -444,7 +444,7 @@ st.markdown(
         }}
 
         .cloud.enlarged .cloud-text {{
-            max-width: 200px;
+            max-width: 360px;
             margin: 0 auto;
             font-size: 14px;
             line-height: 1.7;
