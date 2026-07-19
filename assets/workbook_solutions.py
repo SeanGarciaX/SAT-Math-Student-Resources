9835c7b9e89d538023113a33c40f00ee99d48458
Sean Garcia <sean.garcia2900@gmail.com>
2026-07-19 11:59:00 -0400

    Add workbook solutions for Practice Tests 4, 5, and 6
    Added Google Drive links for Practice Tests 4, 5, and 6.

---
+++ assets/workbook_solutions.py
@@ -0,0 +1,35 @@
+# --------------------------------------------------------------------------
+# WORKBOOK SOLUTIONS - Google Drive links for each Practice Test's PDFs
+#
+# Add a new test's solutions by adding a new entry here with its three
+# module links - no changes needed in notes.py itself. Any test number
+# or module suffix not listed here (or not yet given a link) will show
+# as "Coming Soon" on the Notes page.
+#
+# Each test's three PDFs are keyed by module suffix:
+#   "Module_1"     -> Module 1
+#   "Module_2_HD"  -> Module 2 (Higher Difficulty)
+#   "Module_2_LD"  -> Module 2 (Lower Difficulty)
+#
+# Make sure each Drive file's sharing setting is "Anyone with the link" -
+# otherwise visitors will hit a Google "request access" screen instead
+# of the PDF.
+# --------------------------------------------------------------------------
+
+PRACTICE_TEST_LINKS = {
+    4: {
+        "Module_1": "https://drive.google.com/file/d/1WMgIU-gcfSJXHUn6lSZ3XM6A3hF4vEk6/view?usp=drive_link",
+        "Module_2_HD": "https://drive.google.com/file/d/1VkzaSLsN38N4SNY--wtarj2Nb-xx5I3a/view?usp=drive_link",
+        "Module_2_LD": "https://drive.google.com/file/d/1OqP-VZP5MeKeN3IHqb6Lsg0xikoTfCY2/view?usp=drive_link",
+    },
+    5: {
+        "Module_1": "https://drive.google.com/file/d/1yAsf2EXkAdpuujyPfs5WDz0r4JqGYqvA/view?usp=drive_link",
+        "Module_2_HD": "https://drive.google.com/file/d/18c-1WwODPgNVE7XZePcmd3BusmZxhyNd/view?usp=drive_link",
+        "Module_2_LD": "https://drive.google.com/file/d/1-jsWko08l8rSPzFaWnVtDteqCE38YPn2/view?usp=drive_link",
+    },
+    6: {
+        "Module_1": "https://drive.google.com/file/d/1Gs3yVi9pNHAJdjZhzbt89snWZA-nijsz/view?usp=drive_link",
+        "Module_2_HD": "https://drive.google.com/file/d/1WfVjv1GzO0z0FEc0ugpRJlWyGFCZ07j9/view?usp=drive_link",
+        "Module_2_LD": "https://drive.google.com/file/d/11nDDlsv89UCvrgpCbuLzt68Y0Ex2CU0_/view?usp=drive_link",
+    },
+}
