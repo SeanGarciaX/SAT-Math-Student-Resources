efb13143f698ed0d28d96ded64d281a1f0e892ae
Sean Garcia <sean.garcia2900@gmail.com>
2026-07-19 13:20:18 -0400

    Increase max-width of cloud-container to 500px

--- pages/Testimonials.py
+++ pages/Testimonials.py
@@ -301,7 +301,7 @@ st.markdown(
 
         .cloud-container {{
             width: 100%;
-            max-width: 300px;
+            max-width: 500px;
             display: flex;
             align-items: center;
             justify-content: center;
