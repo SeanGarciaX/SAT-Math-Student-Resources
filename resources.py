cb8bd30fc9aa641ceca57c9a66e6df9859b58322
Sean Garcia <sean.garcia2900@gmail.com>
2026-07-20 10:08:38 -0400

    Add equivalent expression problems to resources
    Added new equivalent expression problems to EXAMPLE_PROBLEMS.

--- resources.py
+++ resources.py
@@ -100,11 +100,13 @@ VIDEO_LINKS = {
     "Radians and Degrees": [
         ("Intro to Radians", "https://www.youtube.com/watch?v=tSsihw-xPHc&t=1s"), 
         ("Converting Radians to Degrees and Vice Versa", "https://www.youtube.com/watch?v=tX6mkpnb_to"),  
-    ]
+    ],
 }
 EXAMPLE_PROBLEMS = {
     # "Topic Name": [("Worksheet Title", "https://link"), ...],
-
+    "Equivalent Expression": [
+        ("Page 359 #12, Page 360 #16, Page 365 #9, Page 374 #15, Page 478 #9, Page 480 #19, Page 598 #3, Page #599 #9, Page 721 #22, Page 725 #11, Page 726 #12, Page 731 #6"),  
+    ],
 }
 KEY_WORDS = {
     # "Topic Name": "text or link",
